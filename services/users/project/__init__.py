e903c64b20e51c3fbaade1e987a414c38efa6fe6
Idi Eradiri <joppa27@users.noreply.github.com>
2019-01-01 19:54:01 -0500

    chore: setup config & docker for local dev (#1)

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,11 +1,13 @@
 
 from flask import Flask, jsonify
+import os
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 @app.route('/users/ping', methods=['GET'])
