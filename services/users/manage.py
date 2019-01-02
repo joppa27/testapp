da343681f82ff3554f611133c459b75baf6fed26
Idi Eradiri <ieradiri@esri.com>
2019-01-02 14:57:25 -0500

    chore: add initial tests

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,5 +1,7 @@
 from flask.cli import FlaskGroup
 from project import app, db
+import unittest
+
 
 cli = FlaskGroup(app)
 
@@ -8,6 +10,15 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
