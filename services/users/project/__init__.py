35d2d0a3b7c6973c7c4fc062bb6e26ded908079f
Idi Eradiri <ieradiri@esri.com>
2019-01-01 21:01:46 -0500

    chore: add db config for local dev

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,8 @@
 
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import os
+
 
 # instantiate the app
 app = Flask(__name__)
@@ -9,6 +11,20 @@ app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
+# instantiate the db
+db = SQLAlchemy(app)
+
+#model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
